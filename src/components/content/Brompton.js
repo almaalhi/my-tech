2ad79753039e7212c69057bfef7dd1fb1749f025
Alex Alvarado <alex.alvarad@gmail.com>
2021-03-17 23:00:29 -0700

    Add Brompton blog content

--- src/components/content/Brompton.js
+++ src/components/content/Brompton.js
@@ -1,13 +1,50 @@
+import pic1 from "../../pictures/brompton/brompton1.jpeg";
+import pic2 from "../../pictures/brompton/brompton2.jpeg";
+
 const Brompton = (props) => {
   return (
-    <div className='content'>
+    <div className="content">
       <h1>My Brompton</h1>
       <div>
-        <p>Content coming soon...</p>
-      </div>
-      <div className='imageDiv'>
-        {/* <img></img> */}
-        <p className='imageDesc'></p>
+        <p>
+          Although I love riding my pint it's not really comparable to riding a
+          bike. Range anxiety and the reliance on software to ensure you don't
+          end up face first in the pavement lead me down the path to also
+          getting a bike. Of course the added benefit of exercise is also great.
+        </p>
+        <p>
+          After deciding to get a bike the next step is deciding which bike.
+          Luckily my wife already had that covered. She had looked into
+          Brompton's (At that time, admittedly I didn't even know what a
+          Brompton was.) for one reason or another. In short, they are very well
+          engineered folding bikes that are reliable and have one of the
+          smallest footprints among folding bikes. It didn't take much
+          convincing for me to fall in love with the concept.
+        </p>
+        <div className="imageDiv">
+          <img className="image" src={pic1} alt="brompton1"></img>
+          <p className="imageDesc"></p>
+        </div>
+        <p>
+          Things have changed quite a bit since March of 2020, but the idea of
+          riding around and just folding our bikes up when we got to a
+          restaurant, store, whatever of our liking and bringing the bike in
+          with us just seemed so cool. I've have a couple bikes stolen in the
+          past, so not having to worry about theft, or at least minimizing it,
+          was very nice.
+        </p>
+        <p>
+          I haven't been able to try it, but supposedly people have successfully
+          folded the bike and stored it in the overhead compartment when
+          traveling by airplane. Once travel is allowed again, it will be nice
+          to be able to arrive at our destination with wheels! And yes a car
+          rental for domestic travel is nice, but riding around on a bike really
+          lets you get a feel for a place.
+        </p>
+        <div className="imageDiv">
+          <img className="image" src={pic2} alt="brompton2"></img>
+          <p className="imageDesc"></p>
+        </div>
       </div>
     </div>
   );
