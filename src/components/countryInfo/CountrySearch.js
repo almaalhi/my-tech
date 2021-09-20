3666f27ca2d45b5b3c94e1c0eb15696ff48ac783
Alex Alvarado <alex.alvarad@gmail.com>
2021-09-19 20:29:12 -0700

    Clear dropdown when submitting or clearing search

--- src/components/countryInfo/CountrySearch.js
+++ src/components/countryInfo/CountrySearch.js
@@ -1,5 +1,6 @@
 import { useState, useContext } from 'react';
 import countryContext from '../../context/country/countryContext';
+import DropdownContext from '../../context/dropdown/dropdownContext';
 
 const CountrySearch = () => {
   const CountryContext = useContext(countryContext);
@@ -14,32 +15,42 @@ const CountrySearch = () => {
     errorMessage,
   } = CountryContext;
 
-  const onSubmit = (e) => {
+  const dropdownContext = useContext(DropdownContext);
+  const { closeDropdown } = dropdownContext;
+
+  const onSubmitHandler = (e) => {
     e.preventDefault();
     if (text === '') {
       setError('Please enter some text and try again');
+      closeDropdown();
     } else {
       getCountries(text);
       setText('');
+      closeDropdown();
     }
   };
 
-  const onChange = (e) => setText(e.target.value);
+  const clearCountriesHandler = () => {
+    clearCountries();
+    closeDropdown();
+  };
+
+  const onChangeHandler = (e) => setText(e.target.value);
 
   return (
     <div className='search'>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmitHandler}>
         <input
           type='text'
           name='text'
           placeholder='Enter Country Name'
           value={text}
-          onChange={onChange}
+          onChange={onChangeHandler}
         />
         <input type='submit' value='Search' className='btn btn-dark' />
 
         {countries.length > 0 && (
-          <button onClick={clearCountries} className='btn btn-light'>
+          <button onClick={clearCountriesHandler} className='btn btn-light'>
             Clear
           </button>
         )}
