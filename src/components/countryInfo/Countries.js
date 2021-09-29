15587d652c987c74476611a3157eda08b90c1510
Alex Alvarado <alex.alvarad@gmail.com>
2021-09-28 22:30:26 -0700

    Update passed props based on new API

--- src/components/countryInfo/Countries.js
+++ src/components/countryInfo/Countries.js
@@ -4,11 +4,13 @@ import countryContext from '../../context/country/countryContext';
 const Countries = ({ info }) => {
   const CountryContext = useContext(countryContext);
 
+  const { setCountry } = CountryContext;
+
   return (
     <div className='country-list-item'>
-      <div onClick={() => CountryContext.setCountry(info.alpha2Code)}>
-        <img src={info.flag} alt='flag' className='flag' />
-        <span className='country-name'>{info.name}</span>
+      <div onClick={() => setCountry(info.cca2)}>
+        <img src={info.flags[0]} alt='flag' className='flag' />
+        <span className='country-name'>{info.name.common}</span>
       </div>
     </div>
   );
