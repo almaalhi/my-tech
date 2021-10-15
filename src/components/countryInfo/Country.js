4915cc00279fc44eaa9acecbb9fc0a2f3e7c086c
Alex Alvarado <alex.alvarad@gmail.com>
2021-10-15 09:33:13 -0700

    Add None text if capital or langauge doesn't exist

--- src/components/countryInfo/Country.js
+++ src/components/countryInfo/Country.js
@@ -13,10 +13,15 @@ const Country = () => {
       <h3>{country.name.common}</h3>
       <img src={country.flags[0]} alt='flag' className='big-flag' />
       <ul className='country-ul'>
-        <li>CAPITAL: {country.capital}</li>
+        <li>CAPITAL: {country.capital ? country.capital : 'None'}</li>
         <li>REGION: {country.region}</li>
         <li className='language-block'>
-          LANGUAGES: <Language languages={country.languages} />
+          LANGUAGES:{' '}
+          {country.languages ? (
+            <Language languages={country.languages} />
+          ) : (
+            'None'
+          )}
         </li>
       </ul>
       <button className='btn btn-dark' onClick={clearCountry}>
