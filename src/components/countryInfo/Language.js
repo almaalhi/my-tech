eaeb3cd56c93717178fd2ddd17cbdbd9fc24e9f2
Alex Alvarado <alex.alvarad@gmail.com>
2022-01-21 21:22:24 -0700

    Remove commented out code from old API

--- src/components/countryInfo/Language.js
+++ src/components/countryInfo/Language.js
@@ -14,33 +14,6 @@ const Language = ({ languages }) => {
       ))}
     </ul>
   );
-
-  //if (props.languages.length === 1) {
-  //return (
-  //<li>
-  //LANGUAGE: {props.languages[0].name}{' '}
-  //{props.languages[0].nativeName !== props.languages[0].name
-  //? `(${props.languages[0].nativeName})`
-  //: null}
-  //</li>
-  //);
-  //} else if (props.languages.length > 1) {
-  //return (
-  //<li>
-  //LANGUAGES:{' '}
-  //<ul>
-  //{props.languages.map((language, ind) => (
-  //<li key={ind}>
-  //{language.name}{' '}
-  //{language.nativeName !== language.name
-  //? `(${language.nativeName})`
-  //: null}
-  //</li>
-  //))}{' '}
-  //</ul>
-  //</li>
-  //);
-  //}
 };
 
 export default Language;
